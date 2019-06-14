473167bb9c20855caeb8f8fa57e9217f84c9c470
unknown <dylansprague11@hotmail.com>
2019-06-14 16:39:59 -0400

    basic pages and routing setup

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Homepage' });
+});
+
+/* GET portfolio page. */
+router.get('/portfolio', function(req, res, next) {
+  res.render('index', { title: 'Portfolio page' });
+});
+
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact page' });
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About Page' });
 });
 
 module.exports = router;
