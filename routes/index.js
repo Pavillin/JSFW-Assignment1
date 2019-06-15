314d32e34fee2ee8206aa25c1fd5280aaae741ff
unknown <dylansprague11@hotmail.com>
2019-06-14 20:06:55 -0400

    Fixed routing errors

--- routes/index.js
+++ routes/index.js
@@ -6,19 +6,26 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Homepage' });
 });
 
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About Page' });
+});
+
 /* GET portfolio page. */
 router.get('/portfolio', function(req, res, next) {
-  res.render('index', { title: 'Portfolio page' });
+  res.render('portfolio', { title: 'Portfolio page' });
+});
+
+/* GET services page. */
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: 'About Page' });
 });
 
 /* GET contact page. */
 router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contact page' });
+  res.render('contact', { title: 'Contact page' });
 });
 
-/* GET about page. */
-router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About Page' });
-});
+
 
 module.exports = router;
